2377eeebc99cc66f553400c89fe6ee2c38ed2d81
Julien <julien.guilet@etu.univ-grenoble-alpes.fr>
2023-03-06 12:51:40 +0100

    Passage à Roquet

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,11 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+#[macro_use] extern crate rocket;
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+fn index() -> &'static str {
+    "Hello, world!"
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
