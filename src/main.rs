d93447fe6acd1389ac4d37613264be14abe99c4c
Julien <julien.guilet@etu.univ-grenoble-alpes.fr>
2023-03-07 08:58:11 +0100

    ajout archi

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-#[macro_use] extern crate rocket;
+mod api;
+mod models;
+mod repository;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
+#[macro_use]
+extern crate rocket;
+
+//add imports below
+use api::user_api::create_user;
+use repository::mongodb_repo::MongoRepo;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    let db = MongoRepo::init();
+    rocket::build().manage(db).mount("/", routes![create_user])
 }
